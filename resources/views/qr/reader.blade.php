2de5f6427b7e300185f7e204f312c990824774ce
sakase29 <sakase29@gmail.com>
2025-03-08 22:44:20 +0900

    Install jsQR

--- resources/views/qr/reader.blade.php
+++ resources/views/qr/reader.blade.php
@@ -1,4 +1,81 @@
 <x-app-layout>
-TEST CODE
+    <div id="wrapper" class="relative">
+        <video id="video" autoplay muted playsinline class="absolute top-0 left-0 invisible"></video>
+        <canvas id="camera-canvas" class="absolute top-0 left-0 z-50"></canvas>
+        <canvas id="rect-canvas" class="absolute top-0 left-0 z-100"></canvas>
+    </div>
+
+    <script src="./jsQR.js"></script>
+    <script>
+        // Webカメラの起動
+        const video = document.getElementById('video');
+        let contentWidth;
+        let contentHeight;
+
+        const media = navigator.mediaDevices.getUserMedia({ audio: false, video: {width:640, height:480} })
+        .then((stream) => {
+            video.srcObject = stream;
+            video.onloadeddata = () => {
+                video.play();
+                contentWidth = video.clientWidth;
+                contentHeight = video.clientHeight;
+                canvasUpdate();
+                checkImage();
+            }
+        }).catch((e) => {
+            console.log(e);
+        });
+        // カメラ映像のキャンバス表示
+        const cvs = document.getElementById('camera-canvas');
+        const ctx = cvs.getContext('2d');
+        const canvasUpdate = () => {
+        cvs.width = contentWidth;
+        cvs.height = contentHeight;
+        ctx.drawImage(video, 0, 0, contentWidth, contentHeight);
+        requestAnimationFrame(canvasUpdate);
+        }
+
+        // QRコードの検出
+        const rectCvs = document.getElementById('rect-canvas');
+        const rectCtx =  rectCvs.getContext('2d');
+        const checkImage = () => {
+        // imageDataを作る
+        const imageData = ctx.getImageData(0, 0, contentWidth, contentHeight);
+        // jsQRに渡す
+        const code = jsQR(imageData.data, contentWidth, contentHeight);
+
+        // 検出結果に合わせて処理を実施
+        if (code) {
+            console.log("QRcodeが見つかりました", code);
+            drawRect(code.location);
+        } else {
+            console.log("QRcodeが見つかりません…", code);
+            rectCtx.clearRect(0, 0, contentWidth, contentHeight);
+        }
+        setTimeout(()=>{ checkImage() }, 500);
+        }
+
+        // 四辺形の描画
+        const drawRect = (location) => {
+        rectCvs.width = contentWidth;
+        rectCvs.height = contentHeight;
+        drawLine(location.topLeftCorner, location.topRightCorner);
+        drawLine(location.topRightCorner, location.bottomRightCorner);
+        drawLine(location.bottomRightCorner, location.bottomLeftCorner);
+        drawLine(location.bottomLeftCorner, location.topLeftCorner)
+        }
+
+        // 線の描画
+        const drawLine = (begin, end) => {
+        rectCtx.lineWidth = 4;
+        rectCtx.strokeStyle = "#F00";
+        rectCtx.beginPath();
+        rectCtx.moveTo(begin.x, begin.y);
+        rectCtx.lineTo(end.x, end.y);
+        rectCtx.stroke();
+        }
+
+
+    </script>
 </x-app-layout>
 
